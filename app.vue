4bfde305cd9d2ade45c2cfeb20a2ed22e7eee5af
yunnn426 <yunnn99@gmail.com>
2024-12-26 20:39:40 +0900

    app.vue

--- app.vue
+++ app.vue
@@ -3,4 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+console.log(nuxtApp);
+
+// vue application instance
+// nuxtApp.vueApp.component() 전역 컴포넌트 등록
+// nuxtApp.vueApp.directive() 디렉티브 정의
+// nuxtApp.vueApp.use() Vue.js 플러그인 설치
+
+// provide 활용
+// nuxtApp.provide('hello', (msg: string) => `Hello ${msg}`);
+</script>
